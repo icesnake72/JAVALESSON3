75b42d1f7a99d0b0046793e62154f3183f870c8a
icesnake72 <icesnake72@gmail.com>
2025-05-11 15:52:33 +0900

    조건문 2형식까지

---
+++ src/basic/ControlStmt.java
@@ -0,0 +1,39 @@
+package basic;
+
+import java.util.Scanner;
+
+public class ControlStmt {
+    public static void main(String[] args) {
+        // if 문
+        /*
+        * 1 형식)
+        * if (조건식 또는 논리식) {
+        *   조건식 또는 논리식이 참인경우 실행되는 명령문들...
+        * }
+        *
+        * 2 형식)
+        * if ( 조건식 또는 논리식) {
+        *   조건식 또는 논리식이 참인경우 실행되는 명령문들...
+        * } else {
+        *   조건식 또는 논리식이 거짓인경우 실행되는 명령문들...
+        * }
+        * */
+        Scanner sc = new Scanner(System.in);
+
+        int age = 0;
+        System.out.print("나이를 입력하세요 >>> ");
+        age = sc.nextInt();
+
+//        if ( age > 18 ) {
+//            System.out.println("당신은 성인입니다");
+//        }
+
+        if ( age > 18 ) {
+            System.out.println("당신은 성인입니다");
+        } else {
+            System.out.println("당신은 성인이 아닙니다");
+        }
+
+        sc.close();
+    }
+}
