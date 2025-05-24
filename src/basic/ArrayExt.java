fcb6870c9b07b46597836d91698e3e5b71802227
icesnake72 <icesnake72@gmail.com>
2025-05-24 15:50:00 +0900

    20250524

---
+++ src/basic/ArrayExt.java
@@ -0,0 +1,73 @@
+package basic;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Random;
+
+public class ArrayExt {
+    public static void main(String[] args) {
+        // primitive type : byte, char, short, int, long, float, double, boolean
+        // referenced type : String, class
+        // Wrapper Class
+        // Byte, Character, Short, Integer, Long, Float, Double, Boolean
+
+        Integer [] arr = new Integer[10];
+
+        Random rand = new Random();
+        for(int i=0; i<arr.length; i++) {
+            arr[i] = rand.nextInt(45)+1;
+        }
+
+//        for(int n : arr) {
+//            System.out.printf("%d\t", n);
+//        }
+        // Arrays.toString(arr) : arr배열을 문자열로 변환
+        System.out.println( Arrays.toString(arr) ); // 디버깅 용도
+
+//        Arrays.sort( arr ); // 오름차순
+//        System.out.println( Arrays.toString(arr) ); // 디버깅 용도
+//
+//        Arrays.sort(arr, Comparator.reverseOrder());    // 내림차순
+//        System.out.println( Arrays.toString(arr) ); // 디버깅 용도
+
+
+        // val을 arr에서 찾아보고 몇번째 인덱스에 있는지 배열과 해당 인덱스값을 출력하는 코드를 작성하세요
+        // 찾았을때)
+        // [ 배열 .... ]
+        // val : xx는 index : yy에 있습니다.
+
+        // 못 찾았을때)
+        // [ 배열 .... ]
+        // val : xx는 배열에 존재하지 않습니다.
+
+        // 배열 전체를 출력 : 검색 결과 유무에 상관없이 출력
+        System.out.println( Arrays.toString(arr) );
+
+        int val = rand.nextInt(45) + 1;
+
+        boolean find = false;   // 검색 성공여부 체크하는 플래그
+        for(int i=0; i<arr.length; i++) {
+            if ( val == arr[i] ) {
+                System.out.printf("val : %d는 인덱스 %d에 있습니다\n", val, i);
+                find = true;
+                break;
+            }
+        }
+
+        if (!find) {
+            System.out.printf("val : %d는 배열에 존재하지 않습니다.\n", val);
+        }
+
+        for(int i=0; i<arr.length-1; i++) {
+            for(int j=i+1; j<arr.length; j++) {
+                if ( arr[i] > arr[j] ) {
+                    int tmp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = tmp;
+                }
+            }
+        }
+
+        System.out.println( Arrays.toString(arr) ); // 디버깅 용도
+    }
+}
--- src/basic/PrintMatrix.java
+++ src/basic/PrintMatrix.java
@@ -5,11 +5,18 @@ public class PrintMatrix {
         for(int i=1; i<=100; i++) {
             int ten = i / 10;
             int il = i % 10;
+            String str = "";
 
-            if (i%3==0)
-                System.out.print("짝\t");
-            else
+            if (ten%3==0 && ten!=0)
+                str = "짝";
+
+            if (il%3==0 && il!=0)
+                str += "짝";
+
+            if (str.isEmpty())  // str이 빈 문자열이면 true를 반환한다.
                 System.out.printf("%d\t", i);
+            else
+                System.out.printf("%s\t", str);
 
             if (i%10 == 0)  // 10단위 체크
                 System.out.println();
